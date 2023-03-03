802e5cea04eb5e5af780d6d51ba9121fff2a3ef8
skillkrio <skillkrio@gmail.com>
2023-03-03 23:28:07 +0530

    feat:add structuring project with clean architecture

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a clean architecture proposed by ResoCoder.
---
+++ android/app/src/main/kotlin/com/example/cleanarchitecture_tdd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cleanarchitecture_tdd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
