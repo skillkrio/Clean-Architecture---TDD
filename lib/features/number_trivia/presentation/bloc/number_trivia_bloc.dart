49573c8835f9b722a15d52e21b8ab8a069bac88a
skillkrio <skillkrio@gmail.com>
2023-04-29 13:09:04 +0530

    feat:add bloc scaffolding and input conversion

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import '../../domain/entities/number_trivia.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  NumberTriviaBloc() : super(Empty()) {
+    on<NumberTriviaEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -0,0 +1,32 @@
+part of 'number_trivia_bloc.dart';
+
+abstract class NumberTriviaEvent extends Equatable {
+  const NumberTriviaEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetTriviaForConcreteNumberEvent extends NumberTriviaEvent {
+  final String numberString;
+  const GetTriviaForConcreteNumberEvent({
+    required this.numberString,
+  });
+
+  @override
+  List<Object> get props => [numberString];
+
+  @override
+  String toString() =>
+      'GetTriviaForConcreteNumberEvent(numberString: $numberString)';
+
+  GetTriviaForConcreteNumberEvent copyWith({
+    String? numberString,
+  }) {
+    return GetTriviaForConcreteNumberEvent(
+      numberString: numberString ?? this.numberString,
+    );
+  }
+}
+
+class GetTriviaForRandomNumberEvent extends NumberTriviaEvent {}
